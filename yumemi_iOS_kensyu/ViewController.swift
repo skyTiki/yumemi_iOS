83e0cf5fbd5fd45a4d8b56b1cde9a8d4cca263bd
takatoshi.ichige <takatoshi.ichige>
2021-04-07 14:00:23 +0900

    Initial Commit

---
+++ yumemi_iOS_kensyu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yumemi_iOS_kensyu
+//
+//  Created by takatoshi.ichige on 2021/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
