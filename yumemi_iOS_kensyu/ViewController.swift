d5dd5ce6eb224e38fd81a6e35db544f0fad3a9dc
takatoshi.ichige <takatoshi.ichige>
2021-04-07 14:35:22 +0900

    session2 の実装

--- yumemi_iOS_kensyu/ViewController.swift
+++ yumemi_iOS_kensyu/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import YumemiWeather
+
+enum weather: String {
+    case sunny,cloudy,rainy
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    @IBAction func tappedReloadButton(_ sender: Any) {
+        
+        let nowWeather =  YumemiWeather.fetchWeather()
+        if let weather = weather.init(rawValue: nowWeather) {
+            switch weather {
+            case .sunny:
+                weatherImageView.image = UIImage(named: "iconmonstr-weather-1.pdf")?.withTintColor(.orange, renderingMode: .alwaysOriginal)
+                weatherImageView.tintColor = .orange
+            case .cloudy:
+                weatherImageView.image = UIImage(named: "iconmonstr-weather-11.pdf")?.withTintColor(.gray, renderingMode: .alwaysOriginal)
+                weatherImageView.tintColor = .lightGray
+            case .rainy:
+                weatherImageView.image = UIImage(named: "iconmonstr-umbrella-1.pdf")?.withTintColor(.blue, renderingMode: .alwaysOriginal)
+                weatherImageView.tintColor = .cyan
+            }
+        }
+    }
+    
+    
+    
 }
 
