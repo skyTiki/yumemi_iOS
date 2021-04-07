c1db9c8f3c0204212cd9098fcde273776cc34764
takatoshi.ichige <takatoshi.ichige>
2021-04-07 15:12:29 +0900

    APIエラーハンドリング

--- yumemi_iOS_kensyu/ViewControllers/ViewController.swift
+++ yumemi_iOS_kensyu/ViewControllers/ViewController.swift
@@ -19,10 +19,29 @@ class ViewController: UIViewController {
     // reloadボタンタップ
     @IBAction func tappedReloadButton(_ sender: Any) {
         
-        let nowWeather =  YumemiWeather.fetchWeather()
-        if let weather = Weather.init(rawValue: nowWeather) {
-            weatherImageView.image = weather.image
+        do {
+            let nowWeather = try YumemiWeather.fetchWeather(at: "")
+            if let weather = Weather.init(rawValue: nowWeather) {
+                weatherImageView.image = weather.image
+            }
+        } catch YumemiWeatherError.invalidParameterError {
+            presentYumemiWeatherAPIAleart(message: "パラメータが誤っております。")
+        } catch YumemiWeatherError.jsonDecodeError {
+            presentYumemiWeatherAPIAleart(message: "JSONの変換に失敗しました。")
+        } catch YumemiWeatherError.unknownError {
+            presentYumemiWeatherAPIAleart(message: "天気情報取得で予期せぬエラーが発生しました。")
+        } catch {
+            presentYumemiWeatherAPIAleart(message: "予期せぬエラーが発生しました。")
         }
+    }
+    
+    private func presentYumemiWeatherAPIAleart(message: String) {
+        let aleart = UIAlertController(title: "天気取得エラー", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        aleart.addAction(okAction)
+        
+        present(aleart, animated: true, completion: nil)
     }
 }
 
